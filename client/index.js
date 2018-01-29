aec1916d4de5eab8a9f2338e5415037caf8fe558
Russ Ward <maddogward@gmail.com>
2018-01-29 10:48:25 -0800

    Fix internal borders to 1 px

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 const WaterWalls = require('./controller').WaterWalls;
-//const views = require('./views');
 const waterWalls = new WaterWalls();
+
 let target = document.getElementById('submit-button');
 target.addEventListener('click', waterWalls.submit);
